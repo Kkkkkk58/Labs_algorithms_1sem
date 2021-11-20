1240550f43be14db6433d766bd7df55376d0fa5d
Karim Khasan <karimhasan3879@gmail.com>
2021-11-21 01:50:58 +0300

    Recursion is to be redone

--- Lab5/task4.py
+++ Lab5/task4.py
@@ -7,24 +7,24 @@ class Node:
         self.left = self.right = None
 
 
-def insert(root, label):
-    if root is None:
+def insert(tree, label):
+    if tree is None:
         return Node(label)
     else:
-        if root.label[0] > label[0]:
-            root.left = insert(root.left, label)
+        if tree.label[0] > label[0]:
+            tree.left = insert(tree.left, label)
         else:
-            root.right = insert(root.right, label)
-    return root
+            tree.right = insert(tree.right, label)
+    return tree
 
 
-def search(root, label):
-    while root is not None and root.label[0] != label:
-        if label < root.label[0]:
-            root = root.left
+def search(tree, label):
+    while tree is not None and tree.label[0] != label:
+        if label < tree.label[0]:
+            tree = tree.left
         else:
-            root = root.right
-    return root.label[1]
+            tree = tree.right
+    return tree.label[1]
 
 
 def quack(command):
@@ -65,17 +65,17 @@ def quack(command):
     elif command[0] == "P":
         if len(command) == 1:
             x = queue.get()
-            print(x)
+            print(x, file=fout)
         else:
             register_number = ord(command[1]) - ord("a")
-            print(registers[register_number])
+            print(registers[register_number], file=fout)
     elif command[0] == "C":
         if len(command) == 1:
             x = queue.get()
-            print(chr(x % 256), end="")
+            print(chr(x % 256), end="", file=fout)
         else:
             register_number = ord(command[1]) - ord("a")
-            print(chr(registers[register_number] % 256), end="")
+            print(chr(registers[register_number] % 256), end="", file=fout)
     elif command[0] == ":":
         return
     elif command[0] == "J":
@@ -105,7 +105,7 @@ def quack(command):
         register_1_number = ord(command[1]) - ord("a")
         register_2_number = ord(command[2]) - ord("a")
         if registers[register_1_number] > registers[register_2_number]:
-            label = command[3:]
+            label = ":" + command[3:]
             command_index = search(labels, label)
             for j in range(command_index + 1, len(commands_array)):
                 quack(commands_array[j])
@@ -117,18 +117,13 @@ def quack(command):
         queue.put(value)
 
 
-def inorder(root):
-    if root:
-        inorder(root.left)
-        print(root.label)
-        inorder(root.right)
-
 fin = open("quack.in")
 fout = open("quack.out", "w")
 commands_array = []
 queue = Queue()
 registers = [0] * 26
 first_insertion = True
+
 for line in fin:
     if line == "\n":
         break
@@ -139,7 +134,9 @@ for line in fin:
             first_insertion = False
         else:
             labels = insert(labels, [line.rstrip("\n"), len(commands_array) - 1])
+
 for command in commands_array:
     quack(command)
+
 fin.close()
 fout.close()
